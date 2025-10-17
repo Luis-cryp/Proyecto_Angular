440ce751ee5ac23cb43a13e02108ba52f334982e
Luis-cryp <luis.claros-v@uniminuto.edu.co>
2025-10-17 01:55:40 -0500

    Se realizo el saludador y el hola mundo

---
+++ angular-hola-mundo/src/app/saludador/saludador.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saludador',
+  standalone: true,
+  templateUrl: './saludador.html',
+  styleUrls: ['./saludador.css']
+})
+export class SaludadorComponent {
+  nombre: string = 'Sebastián';
+}
